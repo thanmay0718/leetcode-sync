e7bb8124101b893718ae49fa163bc0a3e428efd6
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-21 06:17:49 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/219. Contains Duplicate II/Solution.java
+++ LeetCode/Java/Easy/219. Contains Duplicate II/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++ ){
+        for(int i = 0; i < nums.length; i++){
             if(map.containsKey(nums[i])){
-                int prev =  map.get(nums[i]);
+                int prev = map.get(nums[i]);
                 if(i - prev <= k){
                     return true;
                 }
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
         return false;
     }
