40cb1d9fa25e9e224dbbb226e966390bf1ead401
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-21 19:13:49 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/560. Subarray Sum Equals K/Solution.java
+++ LeetCode/Java/Medium/560. Subarray Sum Equals K/Solution.java
@@ -1,14 +1,17 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-    Map<Integer, Integer> map = new HashMap<>();
-    map.put(0,1);
-    int cnt = 0, sum = 0, target = 0;
-        for(int i= 0; i < nums.length; i++){
+        int cnt = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for(int i = 0; i < nums.length; i++){
             sum += nums[i];
-            target = sum - k;
-            if(map.containsKey(target)){
-                cnt += map.get(target);
+
+            int rem = sum - k;
+            if(map.containsKey(rem)){
+                cnt += map.get(rem);
             }
+
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return cnt;
