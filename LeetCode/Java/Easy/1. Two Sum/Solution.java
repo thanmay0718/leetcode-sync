c7afdab8f1e788ebcb3e7c4aa14cd4873eade393
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-21 06:22:53 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/1. Two Sum/Solution.java
+++ LeetCode/Java/Easy/1. Two Sum/Solution.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-
         for(int i = 0; i < nums.length; i++){
-           int need = target - nums[i];
+            int need = target - nums[i];
             if(map.containsKey(need)){
                 return new int[]{map.get(need), i};
-            } else {
-                map.put(nums[i], i);
             }
+            map.put(nums[i], i);
         }
         return new int[]{};
     }
