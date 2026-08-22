4a701006d0ce0a3a8bbbd2b4ccbd234cb993159c
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-22 08:48:48 +0530

    Add Java Solution to Patterns/Linked List

--- LeetCode/Patterns/Linked List/21. Merge Two Sorted Lists/Solution.java
+++ LeetCode/Patterns/Linked List/21. Merge Two Sorted Lists/Solution.java
@@ -10,7 +10,6 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // Brute Force
         ArrayList<Integer> arr = new ArrayList<>();
         ListNode l = list1;
         while(l != null){
