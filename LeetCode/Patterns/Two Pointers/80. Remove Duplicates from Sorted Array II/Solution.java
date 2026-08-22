ce5e4b833058e2e5d058fdc8e9b0ba98f531435a
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-22 15:11:32 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/80. Remove Duplicates from Sorted Array II/Solution.java
+++ LeetCode/Patterns/Two Pointers/80. Remove Duplicates from Sorted Array II/Solution.java
@@ -1,15 +1,8 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int pos = 1;
-        int cnt = 1;
-        for(int i = 1; i <= nums.length - 1; i++){
-            if(nums[i] == nums[i - 1]){
-                cnt++;
-            } else {
-                cnt = 1;
-            }
-
-            if(cnt <= 2){
+        int pos = 2;
+        for(int i = 2; i < nums.length; i++){
+            if(nums[i] != nums[pos - 2]){
                 nums[pos] = nums[i];
                 pos++;
             }
