a14a2d35e66dbb6fcff674cc858cf7c0611c740b
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-22 15:57:15 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/414. Third Maximum Number/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int secMax = Integer.MIN_VALUE;
+        int thirdMax = Integer.MIN_VALUE;
+
+        for(int i : nums){
+            if(i > max){
+                thirdMax = secMax;
+                secMax = max;
+                max = i;
+            } else if (i != max && i >= secMax){
+                thirdMax = secMax;
+                secMax = i;
+            } else if((i != max) && (i != secMax)
+                        && i >= thirdMax ){
+                            thirdMax = i;
+                        }
+
+        }
+        if(thirdMax == Integer.MIN_VALUE){
+            return max;
+        }
+        return thirdMax;
+    }
+}
