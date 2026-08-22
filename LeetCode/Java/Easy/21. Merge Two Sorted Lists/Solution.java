bab7a6f0b7167715b157a9ca17788f42d72a0723
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-22 08:48:37 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/21. Merge Two Sorted Lists/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Brute Force
+        ArrayList<Integer> arr = new ArrayList<>();
+        ListNode l = list1;
+        while(l != null){
+            arr.add(l.val);
+            l = l.next;
+        }
+
+        ListNode r = list2;
+        while(r != null){
+            arr.add(r.val);
+            r = r.next;
+        }
+
+        Collections.sort(arr);
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        for(int i = 0; i < arr.size(); i++){
+            temp.next = new ListNode(arr.get(i));
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+}
