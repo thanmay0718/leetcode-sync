2c8166db15dadfc58ee35980ec47e729ad3f590e
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-23 09:16:24 +0530

    Add Java Solution to Patterns/Linked List

--- LeetCode/Patterns/Linked List/21. Merge Two Sorted Lists/Solution.java
+++ LeetCode/Patterns/Linked List/21. Merge Two Sorted Lists/Solution.java
@@ -10,25 +10,29 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        ListNode l = list1;
-        while(l != null){
-            arr.add(l.val);
-            l = l.next;
-        }
-
-        ListNode r = list2;
-        while(r != null){
-            arr.add(r.val);
-            r = r.next;
-        }
-
-        Collections.sort(arr);
         ListNode dummy = new ListNode(-1);
         ListNode temp = dummy;
-        for(int i = 0; i < arr.size(); i++){
-            temp.next = new ListNode(arr.get(i));
+        ListNode left = list1;
+        ListNode right = list2;
+        while(left != null && right != null){
+            if(left.val < right.val){
+                temp.next = left;
+                left = left.next;
+            } else {
+                temp.next = right;
+                right = right.next;
+            }
             temp = temp.next;
+        }
+
+        if(left != null){
+            temp.next = left;
+            left = left.next;
+        }
+
+        if(right != null){
+            temp.next = right;
+            right = right.next;
         }
         return dummy.next;
     }
