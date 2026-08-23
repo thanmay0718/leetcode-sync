8f702b9b52f49fb3d1c9114fee5ed68c2381fa07
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-23 10:41:59 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/148. Sort List/Solution.java
+++ LeetCode/Patterns/Two Pointers/148. Sort List/Solution.java
@@ -9,23 +9,25 @@
  * }
  */
 class Solution {
+
+    private static ListNode convertedIntoLL(List<Integer> list){
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        for(int i = 0; i < list.size(); i++){
+            temp.next = new ListNode(list.get(i));
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+
     public ListNode sortList(ListNode head) {
-        //YOUR CODE GOES HERE
         List<Integer> list = new ArrayList<>();
         ListNode temp = head;
         while(temp != null){
             list.add(temp.val);
             temp = temp.next;
         }
-
         Collections.sort(list);
-
-        temp = head;
-        int i = 0;
-        while(temp != null){
-            temp.val = list.get(i++);
-            temp = temp.next;
-        }
-        return head;
+        return convertedIntoLL(list);
     }
 }
