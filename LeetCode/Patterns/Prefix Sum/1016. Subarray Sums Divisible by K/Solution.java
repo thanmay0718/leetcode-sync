83325ab1ee78ed1218193b2c796dcb43538ed7e2
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-24 20:32:38 +0530

    Add Java Solution to Patterns/Prefix Sum

--- LeetCode/Patterns/Prefix Sum/1016. Subarray Sums Divisible by K/Solution.java
+++ LeetCode/Patterns/Prefix Sum/1016. Subarray Sums Divisible by K/Solution.java
@@ -1,18 +1,25 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-       Map<Integer, Integer> map = new HashMap<>();
-       map.put(0,1);
-       int cnt = 0;
-       int prefix = 0;
-       for(int i = 0; i < nums.length; i++){
-        prefix += nums[i];
-        // to avoid negative numbers
-        int rem = ((prefix % k) + k) % k;
-        if(map.containsKey(rem)){
-            cnt += map.get(rem);
+        int freq[] = new int[k];
+
+        freq[0] = 1;
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            prefixSum += nums[i];
+
+            int remainder = prefixSum % k;
+
+            if(remainder < 0) {
+                remainder += k;
+            }
+
+            count += freq[remainder];
+            freq[remainder]++;
         }
-        map.put(rem, map.getOrDefault(rem, 0) + 1);
-       } 
-       return cnt;
+
+        return count;
     }
 }
