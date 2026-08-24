38a0047e052128128cc07b769604d738001357eb
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-24 11:43:22 +0530

    Add Java Solution to Patterns/Prefix Sum

--- LeetCode/Patterns/Prefix Sum/523. Continuous Subarray Sum/Solution.java
+++ LeetCode/Patterns/Prefix Sum/523. Continuous Subarray Sum/Solution.java
@@ -10,7 +10,7 @@ class Solution {
                 if(i - map.get(sum % k) >= 2){
                     return true;
                 } else {
-                    map.put(sum, map.getOrDefault(sum, 0) + 1);
+                    map.put(sum, map.getOrDefault(sum, 0);
                 }
             }
         }
