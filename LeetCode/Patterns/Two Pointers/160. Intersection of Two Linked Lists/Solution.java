f5888a02837256218a7b5c36255959f00882e415
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-24 23:42:13 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/160. Intersection of Two Linked Lists/Solution.java
+++ LeetCode/Patterns/Two Pointers/160. Intersection of Two Linked Lists/Solution.java
@@ -11,28 +11,15 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA == null || headB == null){
-            return null;
+        ListNode pA = headA;
+        ListNode pB = headB;
+
+        // Each pointer traverses both lists, meeting at intersection or null
+        while (pA != pB) {
+            pA = (pA != null) ? pA.next : headB;
+            pB = (pB != null) ? pB.next : headA;
         }
 
-        ListNode temp1 = headA;
-        ListNode temp2 = headB;
-
-        while(temp1 != temp2){
-            temp1 = temp1.next;
-            temp2 = temp2.next;
-
-            if(temp1 == temp2){
-                return temp1;
-            }
-
-            if(temp1 == null){
-                temp1 = headB;
-            }
-            if(temp2 == null){
-                temp2 = headA;
-            }
-        }
-        return temp1;
+        return pA;
     }
 }
