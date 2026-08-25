d6af0ef0fecd681542050095e6f565679673028a
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-25 09:40:14 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/838. Design Linked List/Solution.java
+++ LeetCode/Java/Medium/838. Design Linked List/Solution.java
@@ -1,80 +1,93 @@
 class MyLinkedList {
-
-    private class ListNode{
+    // Doubly Linked List
+    private class Node {
         int val;
-        ListNode next;
-        public ListNode (int val){
+        Node prev;
+        Node next;
+        Node(int val) {
             this.val = val;
-            this.next = null;
-        }
-
-        public ListNode (int val, ListNode next){
-            this.val = val;
-            this.next = next;
         }
     }
 
-    private final ListNode head;
+    private Node headSentinel;
+    private Node tailSentinel;
     private int size;
 
     public MyLinkedList() {
-        head = new ListNode(0);
-        size = 0;    
+        headSentinel = new Node(0);
+        tailSentinel = new Node(0);
+        headSentinel.next = tailSentinel;
+        tailSentinel.prev = headSentinel;
+        size = 0;
     }
-    
+
     public int get(int index) {
-        if(index < 0 || index >= size){
-            return -1;
-        }
-
-        ListNode temp = head.next;
-        for(int i = 0; i < index; i++){
-            temp = temp.next;
-        }
-        return temp.val;
+        if (index < 0 || index >= size) return -1;
+        Node node = getNodeAt(index);
+        return node.val;
     }
-    
+
     public void addAtHead(int val) {
-        addAtIndex(0, val);
-    }
-    
-    public void addAtTail(int val) {
-        addAtIndex(size, val);
-    }
-    
-    public void addAtIndex(int index, int val) {
-        if(index > size || index < 0){
-            return;
-        }
-
-        ListNode cur = head;
-        for(int i = 0; i < index; i++){
-            cur = cur.next;
-        }
-        cur.next = new ListNode(val, cur.next);
+        Node newNode = new Node(val);
+        Node after = headSentinel.next;
+        headSentinel.next = newNode;
+        newNode.prev = headSentinel;
+        newNode.next = after;
+        after.prev = newNode;
         size++;
     }
-    
-    public void deleteAtIndex(int index) {
-        if(index >= size || index < 0){
-            return;
-        }
 
-        ListNode curr = head;
-        for(int i = 0; i < index; i++){
-            curr = curr.next;
+    public void addAtTail(int val) {
+        Node newNode = new Node(val);
+        Node before = tailSentinel.prev;
+        before.next = newNode;
+        newNode.prev = before;
+        newNode.next = tailSentinel;
+        tailSentinel.prev = newNode;
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) return;
+        Node successor;
+        Node predecessor;
+        if (index == size) {
+            successor = tailSentinel;
+            predecessor = tailSentinel.prev;
+        } else {
+            successor = getNodeAt(index);
+            predecessor = successor.prev;
         }
-        curr.next = curr.next.next;
+        Node newNode = new Node(val);
+        newNode.prev = predecessor;
+        newNode.next = successor;
+        predecessor.next = newNode;
+        successor.prev = newNode;
+        size++;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+        Node node = getNodeAt(index);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
         size--;
     }
-}
 
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
+    // Helper: traverse from whichever end is closer
+    private Node getNodeAt(int index) {
+        Node node;
+        if (index < size / 2) {
+            node = headSentinel.next;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+        } else {
+            node = tailSentinel.prev;
+            for (int i = size - 1; i > index; i--) {
+                node = node.prev;
+            }
+        }
+        return node;
+    }
+}
