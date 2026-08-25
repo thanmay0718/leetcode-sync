60addcdc7f321d4e640f05f801191180f48cb9cd
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-25 09:38:13 +0530

    Add Java Solution to Patterns/Linked List

---
+++ LeetCode/Patterns/Linked List/838. Design Linked List/Solution.java
@@ -0,0 +1,80 @@
+class MyLinkedList {
+
+    private class ListNode{
+        int val;
+        ListNode next;
+        public ListNode (int val){
+            this.val = val;
+            this.next = null;
+        }
+
+        public ListNode (int val, ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private final ListNode head;
+    private int size;
+
+    public MyLinkedList() {
+        head = new ListNode(0);
+        size = 0;    
+    }
+    
+    public int get(int index) {
+        if(index < 0 || index >= size){
+            return -1;
+        }
+
+        ListNode temp = head.next;
+        for(int i = 0; i < index; i++){
+            temp = temp.next;
+        }
+        return temp.val;
+    }
+    
+    public void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+    
+    public void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index > size || index < 0){
+            return;
+        }
+
+        ListNode cur = head;
+        for(int i = 0; i < index; i++){
+            cur = cur.next;
+        }
+        cur.next = new ListNode(val, cur.next);
+        size++;
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(index >= size || index < 0){
+            return;
+        }
+
+        ListNode curr = head;
+        for(int i = 0; i < index; i++){
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+        size--;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
