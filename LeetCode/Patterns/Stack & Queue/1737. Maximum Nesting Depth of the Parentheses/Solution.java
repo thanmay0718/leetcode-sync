a130ec490c35c1ae40e5c06a037e301fa8011f4d
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-29 21:42:24 +0530

    Add Easy Java Solution for 1737. Maximum Nesting Depth of the Parentheses from LeetCode

---
+++ LeetCode/Patterns/Stack & Queue/1737. Maximum Nesting Depth of the Parentheses/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(String s) {
+        Stack<Character> st = new Stack<>();
+        int max = 0;
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                st.push(ch);
+                max = Math.max(max, st.size());
+            } else if (ch == ')') {
+                st.pop();
+            }
+        }
+        return max;
+    }
+}
