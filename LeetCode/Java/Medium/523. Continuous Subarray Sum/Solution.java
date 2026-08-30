957e4b9f6a5cd4d2ce6b4511df58df6d1e3bb2cf
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-30 09:25:30 +0530

    Add Medium Java Solution for 523. Continuous Subarray Sum from LeetCode

--- LeetCode/Java/Medium/523. Continuous Subarray Sum/Solution.java
+++ LeetCode/Java/Medium/523. Continuous Subarray Sum/Solution.java
@@ -1,13 +1,14 @@
 class Solution {
     // Prefix , Time Complexity : O(N), Space Complexity : O(N)
     public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
-        map.put(0,-1);
+        map.put(0, -1);
         int sum = 0;
         for(int i = 0; i < nums.length; i++){
             sum += nums[i];
             if(map.containsKey(sum % k)){
-                if(map.size() >= 2){
+                if(i - map.get(sum % k) >= 2){
                     return true;
                 }
             } else {
