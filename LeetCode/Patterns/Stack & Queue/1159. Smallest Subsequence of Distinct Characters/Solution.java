469b6ed019e6c1421bbafa74bf4ef11fd284516b
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-08-31 20:11:44 +0530

    Add Medium Java Solution for 1159. Smallest Subsequence of Distinct Characters from LeetCode

---
+++ LeetCode/Patterns/Stack & Queue/1159. Smallest Subsequence of Distinct Characters/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        int[] lastIndex = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            lastIndex[ch - 'a'] = i;
+        }
+
+        boolean[] inStack = new boolean[26];
+        StringBuilder stack = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (inStack[ch - 'a']) {
+                continue;
+            }
+
+            while (stack.length() > 0 &&
+                    stack.charAt(stack.length() - 1) > ch &&
+                    lastIndex[stack.charAt(stack.length() - 1) - 'a'] > i) {
+                inStack[stack.charAt(stack.length() - 1) - 'a'] = false;
+                stack.deleteCharAt(stack.length() - 1);
+            }
+
+            stack.append(ch);
+            inStack[ch - 'a'] = true;
+        }
+        return stack.toString();
+    }
+}
