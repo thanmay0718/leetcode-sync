dd2b6631817b9a603f45f604a00868515d282f09
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-09-08 18:51:25 +0530

    Add Easy Java Solution for 496. Next Greater Element I from LeetCode

---
+++ LeetCode/Java/Easy/496. Next Greater Element I/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+       int[] greaterElements = new int[nums1.length];
+        Arrays.fill(greaterElements, -1);
+
+        for (int i = 0; i < nums1.length; i++) {
+
+            int val = nums1[i];
+
+            // Find val in nums2
+            for (int j = 0; j < nums2.length; j++) {
+
+                if (nums2[j] == val) {
+
+                    // Search for next greater element
+                    for (int k = j + 1; k < nums2.length; k++) {
+
+                        if (nums2[k] > val) {
+                            greaterElements[i] = nums2[k];
+                            break;
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+        return greaterElements; 
+    }
+}
--- LeetCode/STRIVER_SDE.md
+++ LeetCode/STRIVER_SDE.md
@@ -3,7 +3,7 @@
 Track your progress on the Striver's SDE Sheet top interview problems.
 
 ## Progress
-- **Completed:** 9 / 76 (11.8%)
+- **Completed:** 10 / 76 (13.2%)
 
 ---
 
@@ -63,7 +63,7 @@ Track your progress on the Striver's SDE Sheet top interview problems.
 
 ### 📂 Stack & Queue
 - [x] [Valid Parentheses](./Java/Easy/20. Valid Parentheses/)
-- [ ] Next Greater Element I
+- [x] [Next Greater Element I](./Java/Easy/496. Next Greater Element I/)
 - [ ] Next Greater Element II
 - [ ] Largest Rectangle in Histogram
 - [ ] Sliding Window Maximum
