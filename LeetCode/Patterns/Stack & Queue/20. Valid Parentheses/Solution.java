679a0b85581bd8b78ac5f5d924ffdf975c594ffc
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-09-16 18:30:59 +0530

    Add Easy Java Solution for 20. Valid Parentheses from LeetCode

--- LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.java
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.java
@@ -1,28 +1,35 @@
 class Solution {
-
-    private boolean matchCheck(char open, char close){
+    // Time Complexity : O(n)
+    // Space Complexity : O(n) 
+    private boolean isMatched(char open, char close){
         if((open == '(' && close == ')') ||
             (open == '{' && close == '}') ||
-            (open == '[' && close == ']')
-        ){
-            return true;
-        }
+            (open == '[' && close == ']'))
+            {
+                return true;
+            }
         return false;
     }
+
     public boolean isValid(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
+        Stack< Character > st = new Stack<>();
         for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            if(ch == '(' || ch == '{' || ch == '['){
-                stack.push(ch);
+            if((s.charAt(i) == '(') ||
+               (s.charAt(i) == '{') ||
+               (s.charAt(i) == '['))
+            {
+                st.push(s.charAt(i));
             } else {
-                char open = stack.peek();
-                stack.pop();
-                if(!matchCheck(open, ch)){
+                if(st.isEmpty()){
+                    return false;
+                }
+                char open = st.peek();
+                st.pop();
+                if(!isMatched(open, s.charAt(i))){
                     return false;
                 }
             }
         }
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
