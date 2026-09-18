f30624c0fcbdd09933767dedb304e34947955266
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-09-18 19:01:45 +0530

    Add Easy Java Solution for 383. Ransom Note from LeetCode

--- LeetCode/Java/Easy/383. Ransom Note/Solution.java
+++ LeetCode/Java/Easy/383. Ransom Note/Solution.java
@@ -1,19 +1,5 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] d = new int[26];
-        for(int i = 0; i < ransomNote.length(); i++){
-            d[ransomNote.charAt(i) - 'a']++;
-        }
-
-        for(int i = 0; i < magazine.length(); i++){
-            d[magazine.charAt(i) - 'a']--;
-        }
-
-        for(int i = 0; i < d.length; i++){
-            if(d[i] > 0){
-                return false;
-            }
-        }
-        return true;
+        return magazine.contains(ransomNote);
     }
 }
