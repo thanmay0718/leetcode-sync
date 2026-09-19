3a3960e440876e4f305f4a6ffb67a328824e70a5
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-09-20 00:15:56 +0530

    Add Easy Java Solution for 782. Jewels and Stones from LeetCode

--- LeetCode/Java/Easy/782. Jewels and Stones/Solution.java
+++ LeetCode/Java/Easy/782. Jewels and Stones/Solution.java
@@ -1,13 +1,19 @@
 class Solution {
+    // Optimized verion : O(n) both space and time 
     public int numJewelsInStones(String jewels, String stones) {
-        int count = 0;
-        for (int i = 0; i < jewels.length();i++){
-            for ( int j=0;j < stones.length();j++){
-                if(stones.charAt(j) == jewels.charAt(i)) {
-                    count++;
-                }
+        int cnt = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < stones.length(); i++){
+            char ch = stones.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(int i = 0; i < jewels.length(); i++){
+            char ch = jewels.charAt(i);
+            if(map.containsKey(ch)){
+                cnt += map.get(ch);
             }
         }
-        return count;
+        return cnt;
     }
 }
