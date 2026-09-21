470b27e83b8a00556de5749c4f81c34cac947a56
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-09-21 19:12:07 +0530

    Add Medium Java Solution for 907. Sum of Subarray Minimums from LeetCode

--- LeetCode/Patterns/Dynamic Programming/907. Sum of Subarray Minimums/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/907. Sum of Subarray Minimums/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     // Brute Force O(n^2)
     public int sumSubarrayMins(int[] arr) {
         long min = 0;
-        int mod = 1_000_000_007;
+        long mod = 1_000_000_007L;
         for(int i = 0; i < arr.length; i++){
             int curr = arr[i];
             for(int j = i; j < arr.length; j++){
