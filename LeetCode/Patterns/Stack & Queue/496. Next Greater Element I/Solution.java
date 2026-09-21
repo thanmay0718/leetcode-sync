192b609d67cf6255e245dc2db8d1261df2473aaa
Racha. Tanmay Sri Vardhan <rachatanmay0718@gmail.com>
2026-09-21 19:53:35 +0530

    Add Easy Java Solution for 496. Next Greater Element I from LeetCode

--- LeetCode/Patterns/Stack & Queue/496. Next Greater Element I/Solution.java
+++ LeetCode/Patterns/Stack & Queue/496. Next Greater Element I/Solution.java
@@ -1,21 +1,22 @@
 class Solution {
-    public int[] nextLargerElement(int[] arr) {
-        int[] nxtGre = new int[arr.length];
-        Deque<Integer> stack = new ArrayDeque<>();
-        for(int i = arr.length - 1; i >= 0; i--){
-            int val = arr[i];
-            while(!stack.isEmpty() && stack.peek() <= val){
-                stack.pop();
-            }
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+       int[] greaterElements = new int[nums1.length];
+        Arrays.fill(greaterElements, -1);
 
-            if(stack.isEmpty()){
-                nxtGre[i] = -1;
-            } else {
-                nxtGre[i] = stack.peek();
+        for (int i = 0; i < nums1.length; i++) {
+            int val = nums1[i];
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j] == val) {
+                    for (int k = j + 1; k < nums2.length; k++) {
+                        if (nums2[k] > val) {
+                            greaterElements[i] = nums2[k];
+                            break;
+                        }
+                    }
+                    break;
+                }
             }
-
-            stack.push(val);
         }
-        return nxtGre;
+        return greaterElements; 
     }
 }
